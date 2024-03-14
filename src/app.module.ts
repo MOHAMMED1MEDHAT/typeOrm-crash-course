228acb71445ecab54e5768a31ee2fcb386b3dc20
mohammed medhat <mohamed.medhat2121@gmail.com>
2024-03-14 13:29:34 +0200

    feat(db):adding presistance layer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigsModule } from './configs/configs.module';
+import { typeOrmConfig } from './configs/typeorm.config';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [ConfigsModule, TypeOrmModule.forRoot(typeOrmConfig)],
+	controllers: [],
+	providers: [],
 })
 export class AppModule {}
